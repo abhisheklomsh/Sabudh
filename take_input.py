f6b880f5618e253a9cff4190e1cdd26f4cb06d2d
abhisheklomsh <abhisheklomsh@gmail.com>
2019-01-08 10:24:58 +0530

    8/01/2019

---
+++ take_input.py
@@ -0,0 +1,3 @@
+#age = input('Hi! Please enter your current age and press \'Enter\'')
+years = 100-int(19)
+print('You will turn 100 in '+str(years)+' years')
---
+++ untitled.py
@@ -0,0 +1,6 @@
+num = input('Hi! Please enter the number to be checked and press \'Enter\'')
+e_o_o = num%2
+if e_o_o == 1
+	print(str(num)+' is odd')
+else
+	print(str(num)+' is even')
