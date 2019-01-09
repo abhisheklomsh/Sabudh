ebb6929f25105cd628fea78406f2efc2fdae0287
abhisheklomsh <abhisheklomsh@gmail.com>
2019-01-09 16:37:55 +0530

    9/01/2019

---
+++ guessing_game_one.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  9 11:21:54 2019
+
+@author: abhisheklomsh
+"""
+import random
+def guessing_game_one(guessed_num):
+    correct_num=random.randint(1,9)
+    turn=0
+    code=''
+    while code!='exit':
+        if(guessed_num>correct_num):
+            print("You guess is too high!")
+            
+            turn+=1
+            code=input("Enter exit to quit or y to try again!")
+            if code != 'exit':
+                guessed_num=int(input("Enter exit to quit or try again!"))
+        elif(guessed_num<correct_num):
+            print("You guess is too low!")
+            code=input("Enter exit to quit or y to try again!")
+            if code != 'exit':
+                guessed_num=int(input("Enter exit to quit or try again!"))
+            turn+=1
+        elif(guessed_num==correct_num):
+            print("You guessed it right! \n It took you ",turn," attempt(s)")
+            code=input("Enter exit to quit or y to start again!")
+            if code != 'exit':
+                correct_num=random.randint(1,9)
+                turn=0
+    if code=='exit':
+        print('correct answer was ',correct_num)
+    
+guessing_game_one(int(input('Enter your guessed number')))
+        
---
+++ prime_checker.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  9 10:51:47 2019
+
+@author: abhisheklomsh
+"""
+
+def prime_check(input_num):
+    flag=1
+    if input_num ==1: print(str(input_num)+" is not a prime number")
+    else:
+        for i in range(2,input_num+1):
+            if (input_num==i and flag ==1):
+                print(str(input_num)+" is a prime number!")
+                pass
+            elif(input_num%i==0 and flag==1):
+                print("This is not a prime number")
+                flag=0
+                
+
+prime_check(int(input('Enter number to be cheked if it\'s prime or not')))
+    
