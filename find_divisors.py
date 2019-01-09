b612f62f2bb8a6095269cbb7c4ec1a8f10811d8b
abhisheklomsh <abhisheklomsh@gmail.com>
2019-01-09 10:22:59 +0530

    9/01/2019

--- find_divisors.py
+++ find_divisors.py
@@ -1,6 +1,6 @@
 num = int(input('Hi! Please enter the number to find divisors and press \'Enter\''))
 mylist = []
-for div in range(2,num):
+for div in range(2,int(num/2)):
 	if num%div == 0:
 		mylist.append(div)
 	else:
