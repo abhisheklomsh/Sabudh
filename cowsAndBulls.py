66b7125afc51ab891070861dabf7869b9abe11f8
abhisheklomsh <abhisheklomsh@gmail.com>
2019-01-16 10:50:49 +0530

    16/01/2019

--- cowsAndBulls.py
+++ cowsAndBulls.py
@@ -4,9 +4,16 @@
 Created on Wed Jan  9 17:17:54 2019
 
 @author: abhisheklomsh
+
+Create a program that will play the “cows and bulls” game with the user. The game works like this:
+
+Randomly generate a 4-digit number. Ask the user to guess a 4-digit number. 
+For every digit that the user guessed correctly in the correct place, they have a “cow”. 
+For every digit the user guessed correctly in the wrong place is a “bull.” 
+Every time the user makes a guess, tell them how many “cows” and “bulls” they have. 
+Once the user guesses the correct number, the game is over. Keep track of the number of guesses the user makes throughout teh game and tell the user at the end.
 """
 
-import random
 import numpy
 flag=0
 cow=0
@@ -34,8 +41,10 @@ while cow!=4:
                 print(str(guess)[iteratn])
                 bull+=1
             flag+=1
+        break
         
     print("You got cow ", cow," and bulls ",bull)
-    bull=0
-    cow=0
-    pos_marker=[]
+    
+    #bull=0
+    #cow=0
+    #pos_marker=[]
---
+++ string_input.py
@@ -0,0 +1,8 @@
+"""
+author: Abhishek Lomsh
+Here I am trying to get a string from user and print it
+    """
+
+string = input('Hi please pass a word or sentence')
+print(string)
+print(len(string))
