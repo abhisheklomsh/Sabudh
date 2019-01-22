02962228129bcb4b7cf3ae64ae506bb61c9309c8
abhisheklomsh <abhisheklomsh@gmail.com>
2019-01-22 10:11:15 +0530

    22/01/2019

---
+++ iris_knn.py
@@ -0,0 +1,12 @@
+
+from sklearn import datasets
+import pandas as pd
+
+
+df = pd.read_csv("/Users/abhisheklomsh/Repositories/Sabudh/iris-species/iris.csv")
+print(df.head(10))
+print(df.shape)
+
+
+
+
